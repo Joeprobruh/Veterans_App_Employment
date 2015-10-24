eb2c0b0a314d9979a73fc22523ae3f1d14ef8d40
joe-proleika <proleikaj6@students.rowan.edu>
2015-10-24 17:02:08 -0400

    Began adding XML activity files. JJP

---
+++ src/com/Senior_Proj_Fall_2015/Veterans_App_Employment/LoadScreen.java
@@ -0,0 +1,51 @@
+package com.Senior_Proj_Fall_2015.Veterans_App_Employment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Joe on 10/24/2015.
+ */
+public class LoadScreen extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(LoadScreen.this, StartPage.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
+    }
+
+}
--- src/com/Senior_Proj_Fall_2015/Veterans_App_Employment/StartPage.java
+++ src/com/Senior_Proj_Fall_2015/Veterans_App_Employment/StartPage.java
@@ -1,16 +1,54 @@
 package com.Senior_Proj_Fall_2015.Veterans_App_Employment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Joe on 10/17/2015.
  */
-public class StartPage {
+public class StartPage extends Activity implements View.OnClickListener {
 
-    /*
-    The very first screen the user sees.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
 
-    Two buttons
-        -Login: redirects to Login
-        -Sign up: redirects to SignUpPage
-     */
+        Button login_OK =
+            (Button) findViewById(R.id.login_OK);
+        login_OK.setOnClickListener(this);
+
+        Button sign_up_OK =
+            (Button) findViewById(R.id.sign_up_OK);
+        sign_up_OK.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.login_OK:
+                Intent i = new Intent(
+                    StartPage.this, Login.class);
+                startActivity(i);
+                break;
+
+            case R.id.sign_up_OK:
+                Intent j = new Intent(
+                    StartPage.this, SignUpPage.class);
+                startActivity(j);
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
+    }
 
 }
